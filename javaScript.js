95cabdc25d0bafa74640ebcf087c77d161494987
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-06-30 13:45:39 +0000

    build function to show a random quote and its author

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
     
-    let quotesArray;
+    let allQuotes;
+    let randomValue;
 
     $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
             success: function (allquotes) {
-                console.log(allquotes);
-                quotesArray = JSON.parse(allquotes);
-                console.log(quotesArray.quotes[0].quote);
+                allQuotes = JSON.parse(allquotes);
+                randomValue = getRandomValue(allQuotes.quotes.length);
+                writeNewQuote(randomValue);
             } 
-        });
-    })
+    });
+
+    function getRandomValue(length) {
+        return Math.floor(Math.random() * length);
+    }
+    
+    function writeNewQuote(value) {
+        $("#text").html(allQuotes.quotes[value].quote);
+        $("#author").html(allQuotes.quotes[value].author);
+    }
+
+})
 
 
