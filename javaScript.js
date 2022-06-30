8c321fb01952299c26fd23f1c1913202849745c5
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-06-30 14:05:06 +0000

    add an onclick event to the button to change quote

--- javaScript.js
+++ javaScript.js
@@ -1,25 +1,28 @@
-$(document).ready(function() {
-    
-    let allQuotes;
-    let randomValue;
+let allQuotes;
 
+function getRandomValue(length) {
+    return Math.floor(Math.random() * length);
+}
+
+function writeNewQuote(value) {
+    $("#text").text(allQuotes.quotes[value].quote);
+    $("#author").text(allQuotes.quotes[value].author);
+}
+
+function newQuote() {
     $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
-            success: function (allquotes) {
-                allQuotes = JSON.parse(allquotes);
-                randomValue = getRandomValue(allQuotes.quotes.length);
-                writeNewQuote(randomValue);
-            } 
+        success: function (allquotes) {
+            allQuotes = JSON.parse(allquotes);
+            writeNewQuote(getRandomValue(allQuotes.quotes.length));
+        } 
     });
+}
 
-    function getRandomValue(length) {
-        return Math.floor(Math.random() * length);
-    }
-    
-    function writeNewQuote(value) {
-        $("#text").html(allQuotes.quotes[value].quote);
-        $("#author").html(allQuotes.quotes[value].author);
-    }
-
-})
+$(function() {
+    newQuote();
+    $("#new-quote").on('click', function() {
+        newQuote();
+    });
+});
 
 
