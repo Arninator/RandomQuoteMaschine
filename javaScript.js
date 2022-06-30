209df6b080c0ff00d0b2c6846995564adccf7ad7
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-06-30 13:14:51 +0000

    ...

---
+++ javaScript.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    let quotesArray;
+
+    $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+            success: function (allquotes) {
+                console.log(allquotes);
+                quotesArray = JSON.parse(allquotes);
+                console.log(quotesArray.quotes[0].quote);
+            } 
+        });
+    })
+
+
