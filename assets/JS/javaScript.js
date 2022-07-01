b743f04a29aafa10fbea76cb9cacc5c8c83395d0
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-01 14:47:39 +0000

    change layout, design completely, modify js to make design happen

--- assets/JS/javaScript.js
+++ assets/JS/javaScript.js
@@ -13,7 +13,20 @@ function newQuote() {
     $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
         success: function (allquotes) {
             allQuotes = JSON.parse(allquotes);
-            writeNewQuote(getRandomValue(allQuotes.quotes.length));
+            let randomValue = getRandomValue(allQuotes.quotes.length);;
+
+            $("#quote-box").html("<div id='quote-box'></div>");
+
+            for (let i = 0; i < allQuotes.quotes.length; i++) {
+                if (i == randomValue) {
+                    $("#quote-box").append("<div id='text'><span style='font-size:75px;'>\"  </span>" + allQuotes.quotes[i].quote + "<span style='font-size:25px;'>&nbsp;&nbsp;" + allQuotes.quotes[i].author + "</span></div>")
+                    //$("#quote-box").append("<div id='text'><span style='font-size:50px;'>" + allQuotes.quotes[i].quote + "</span>" + "<span style='font-size:25px;'>&nbsp;&nbsp;" + allQuotes.quotes[i].author + "</span></div>")
+                } else {
+                    $("#quote-box").append(allQuotes.quotes[i].quote)
+                    //console.log(i + ": " + allQuotes.quotes[i].quote);
+                }
+                
+            }
         } 
     });
 }
